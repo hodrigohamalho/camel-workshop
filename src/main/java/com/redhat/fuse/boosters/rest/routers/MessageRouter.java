3f89b81e43aac0f0b295339e34bdac4da805ab3a
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-01-31 09:20:13 -0200

    messaging part

--- src/main/java/com/redhat/fuse/boosters/rest/routers/MessageRouter.java
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/MessageRouter.java
@@ -10,19 +10,13 @@ public class MessageRouter extends RouteBuilder{
 	@Override
 	public void configure() throws Exception {
 
-		// mock a order creation
 		from("direct:create-order")
 			.log("Sending Order to AMQ broker: ${body}")
 			.to(ExchangePattern.InOnly, "activemq:queue:backend");
 
+		// Place your code here...
 		from("activemq:queue:backend")
-			.log("Message received from the AMQ broker: ${body}")
-			.log("Sending message to send-email queue")
-			.to(ExchangePattern.InOnly, "activemq:queue:send-email");
-
-		from("activemq:queue:send-email")
-			.log("Sending email...")
-			.to("mock:smtp");
+			.log("Continue your code here...");
 		
 	}
 
