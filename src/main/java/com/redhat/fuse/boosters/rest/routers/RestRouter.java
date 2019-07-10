9801cab7763a63262b40a5554393f27acb7e9133
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-07-10 03:31:15 -0300

    prepare REST with database lab

--- src/main/java/com/redhat/fuse/boosters/rest/routers/RestRouter.java
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/RestRouter.java
@@ -1,43 +1,41 @@
-// package com.redhat.fuse.boosters.rest.routers;
+package com.redhat.fuse.boosters.rest.routers;
 
-// import org.apache.camel.builder.RouteBuilder;
-// import org.springframework.stereotype.Component;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
-// import com.redhat.fuse.boosters.rest.model.Greetings;
-// import com.redhat.fuse.boosters.rest.model.Order;
+import com.redhat.fuse.boosters.rest.model.Order;
 
-// @Component
-// public class RestRouter extends RouteBuilder {
+@Component
+public class RestRouter extends RouteBuilder {
 
-//     @Override
-//     public void configure() throws Exception {
+    @Override
+    public void configure() throws Exception {
 
-//         rest("/orders").description("Content Base Router Example")
+        rest("/orders").description("Order rest lab")
 
-//             .get("/pay") // yes, it should be post... but let's thing more didact here :)
-//                 .route().routeId("content-base-router")
-//                 .to("direct:cbr")
-//                 .endRest()
+            .get("/")
+                .route().routeId("get-all-orders")
+                .log("geting all orders entries from database")
+                .to(this.selectAll)
+                .endRest()
 
-//             .get("/")
-//                 .route().routeId("get-all-orders")
-//                 .to("direct:get-all-orders")
-//                 .endRest()
+            .get("/{id}")
+                .route().routeId("get-order-by-id")
+                .log("geting order with id ${header.id} entry from database")
+                .to(this.selectById)
+                .endRest()
         
-//             .post("/").type(Order.class).description("Create a new Order")
-//             	.route().routeId("insert-order").tracing()
-//             	.log("Order received")
-//             	.wireTap("direct:create-order")
-// 				.setBody(constant("Your request was sent, thank you... Your confirmation will be sent by email."));
+            .post("/").type(Order.class).description("Create a new Order")
+            	.route().routeId("insert-order").tracing()
+            	.log("Order received")
+            	.wireTap("direct:create-order")
+				.setBody(constant("Your request was sent, thank you... Your confirmation will be sent by email."));
+ 
+    }
 
-//         rest("/greetings").description("Greeting to {name}")
-//             .get("/{name}").outType(Greetings.class)
-//                 .route().routeId("greeting-api")
-//                 .to("direct:greetingsImpl");
+    // Query support
+    private String ds = "?dataSource=dataSource";
+    private String selectAll = "sql:select * from orders" + ds;
+    private String selectById = "sql:select * from orders where id = :#${header.id}"  + ds;
 
-//         from("direct:greetingsImpl").description("Greetings REST service implementation route")
-//             .streamCaching()
-//             .to("bean:greetingsService?method=getGreetings");     
-//     }
-
-// }
+}
