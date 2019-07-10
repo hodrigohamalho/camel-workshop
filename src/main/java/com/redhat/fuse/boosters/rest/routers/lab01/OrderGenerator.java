aeece2f7b3bf7b8200a06cd8de7494b221c2a742
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-07-10 01:45:03 -0300

    adding processor step

--- src/main/java/com/redhat/fuse/boosters/rest/routers/lab01/OrderGenerator.java
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/lab01/OrderGenerator.java
@@ -1,10 +1,7 @@
 package com.redhat.fuse.boosters.rest.routers.lab01;
 
-import com.redhat.fuse.boosters.rest.model.Order;
 import com.redhat.fuse.boosters.rest.service.OrderService;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -28,27 +25,17 @@ public class OrderGenerator extends RouteBuilder {
                     .to("file:/tmp/fuse-workshop/camel?fileName=camel-${date:now:yyyy-MM-dd-HHmmssSSS}.json")
                 .otherwise()
                     .log("Processing an activemq book")
+                    .process(new OrderProcessor())
                     .marshal().jacksonxml()
                     .to("file:/tmp/fuse-workshop/activemq?fileName=activemq-${date:now:yyyy-MM-dd-HHmmssSSS}.xml");
 
         from("file:/tmp/fuse-workshop/activemq?delete=true")
             .log("uploading activemq orders to ftp")
-            .unmarshal().jacksonxml(Order.class)
-            .process(new Processor(){
-                @Override
-                public void process(Exchange exchange) throws Exception {
-                    Order order = exchange.getIn().getBody(Order.class);
-                    order.setProcessed(true);
-                    exchange.getOut().setBody(order);
-                }
-            })
-            .setHeader("CamelFileName", simple("activemq-${date:now:yyyy-MM-dd-HHmmssSSS}.xml"))
-            .marshal().jacksonxml()
-        .to("ftp://evals01@52.15.235.138?password=Password1&localWorkDirectory=/var/fuse-workshop/evals01");
+        .to("ftp://<ftp-username>@<ftp-host>?password=<ftp-password>&localWorkDirectory=<ftp-path>");
 
         from("file:/tmp/fuse-workshop/camel?delete=true")
             .log("uploading camel orders to ftp")
-        .to("ftp://evals01@52.15.235.138?password=Password1&localWorkDirectory=/var/fuse-workshop/evals01");
+        .to("ftp://<ftp-username>@<ftp-host>?password=<ftp-password>&localWorkDirectory=<ftp-path>");
     }
 
 }
---
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/lab01/OrderProcessor.java
@@ -0,0 +1,17 @@
+package com.redhat.fuse.boosters.rest.routers.lab01;
+
+import com.redhat.fuse.boosters.rest.model.Order;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class OrderProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Order order = exchange.getIn().getBody(Order.class);
+        order.setProcessed(true);
+        exchange.getOut().setBody(order);
+	}
+
+}
