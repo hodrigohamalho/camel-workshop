9671e91b73a035e49eeda9efd3bdb772e408b9e9
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-07-16 09:25:17 -0300

    empting the class impl

--- src/main/java/com/redhat/fuse/boosters/rest/routers/lab02/RestRouter.java
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/lab02/RestRouter.java
@@ -11,43 +11,7 @@ public class RestRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        rest("/orders").description("Orders CRUD REST endpoint")
-
-            .get("/").description("Get all orders")
-                .route().routeId("all-orders")
-                .log("geting all orders entries from database")
-                .to(this.selectAll)
-                .endRest()
-
-            .get("/{id}").description("Get orders by id")
-                .route().routeId("find-by-id")
-                .log("geting order with id ${header.id} entry from database")
-                .to(this.selectById)
-                .endRest()
-                
-            .post("/").type(Order.class).description("Create a new order")
-                .route().routeId("create order")
-                .log("Order received")
-                .to(this.insertOrder)
-                .endRest()
-
-            .post("/async").type(Order.class).description("Create a new order async")
-                .route().routeId("create-order-async")
-                .log("Order received")
-                .wireTap("direct:create-order")
-                .setBody().simple("We received your request, as soon we process your request we will notify you by email.")
-                .endRest();
-
-            // Produce a message to the broker
-            from("direct:create-order")
-                .log("sending ${body.item} to JMS queue")
-                .to("activemq:queue:orders");
-
-            // Consume from the message broker queue
-            from("activemq:queue:orders")
-                .log("received ${body.item} from JMS queue")
-                .to(this.insertOrder)
-                .to("mock:notify-by-email");
+        // PUT YOUR CODE HERE
  
     }
 
