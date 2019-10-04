e5b2ea076a639cb382185e4bb86982352b45fedd
Eric Deandrea <edeandrea@redhat.com>
2019-10-04 08:02:01 -0400

    Update to Fuse 7.4/SB 2

--- src/main/java/com/redhat/fuse/boosters/rest/setup/RestSetup.java
+++ src/main/java/com/redhat/fuse/boosters/rest/setup/RestSetup.java
@@ -2,6 +2,7 @@ package com.redhat.fuse.boosters.rest.setup;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +12,17 @@ public class RestSetup extends RouteBuilder{
     public void configure() throws Exception {
 
         restConfiguration()
-            .apiContextPath("/api-doc")
-            .apiProperty("api.title", "Greeting REST API")
-            .apiProperty("api.version", "1.0")
-            .apiProperty("cors", "true")
-            .apiProperty("base.path", "camel/")
-            .apiProperty("api.path", "/")
-            .apiProperty("host", "")
-            .apiContextRouteId("doc-api")
-        .component("servlet")
-        .bindingMode(RestBindingMode.json);
-    
+          .apiContextPath("/api-doc")
+          .apiProperty("api.title", "Greeting REST API")
+          .apiProperty("api.version", "1.0")
+          .apiProperty("cors", "true")
+          .apiProperty("base.path", "camel/")
+          .apiProperty("api.path", "/")
+          .apiProperty("host", "")
+          .apiProperty("schemes", "https,http")
+          .apiContextRouteId("doc-api")
+          .component("servlet")
+          .bindingMode(RestBindingMode.json);
+
     }
 }
