252443f83a6e927f5d20d54fd88c2924f226fbbb
Guilherme Camposo <guilherme.camposo@me.com>
2021-09-16 12:03:39 -0300

    fixing clean template

--- src/main/java/com/redhat/workshop/fuse/routers/OrderGenerator.java
+++ src/main/java/com/redhat/workshop/fuse/routers/OrderGenerator.java
@@ -1,8 +1,8 @@
 package com.redhat.workshop.fuse.routers;
 
 import org.apache.camel.builder.RouteBuilder;
-
 import org.springframework.stereotype.Component;
+import com.redhat.workshop.fuse.service.OrderService;
 
 @Component
 public class OrderGenerator extends RouteBuilder {
