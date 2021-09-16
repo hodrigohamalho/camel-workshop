9065b71d4104c8da74f81182817e4c1b5b562cfc
Guilherme Camposo <guilherme.camposo@me.com>
2021-09-16 08:52:32 -0300

    adding complete code

--- src/main/java/com/redhat/fuse/boosters/rest/routers/lab01/OrderProcessor.java
+++ src/main/java/com/redhat/fuse/boosters/rest/routers/lab01/OrderProcessor.java
@@ -10,10 +10,11 @@ public class OrderProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         Order order = exchange.getIn().getBody(Order.class);
-        
-        // set order process to true here
 
-        
+        order.setProcessed(true);
+        System.out.println("attributed process changed");
+
+
         exchange.getOut().setBody(order);
 	}
 
